7081e04b1ed050cd449cc93c8b007089f9f32934
Jithendranath8 <145124590+Jithendranath8@users.noreply.github.com>
2026-04-30 07:16:09 +0530

    chore: add next.config.ts

---
+++ frontend/next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  allowedDevOrigins: ['10.7.0.202', 'localhost:3000', 'localhost:3002']
+};
+
+export default nextConfig;
